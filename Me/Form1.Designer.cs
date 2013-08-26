8607f1278d2c7f4d6db2f36265497cdb95e24982
Jeff Moore <jeff.moore@amway.com>
2013-08-26 15:24:18 -0400

    Added Remove Button

---
+++ Me/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Me
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.lstBox1 = new System.Windows.Forms.ListBox();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(63, 66);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.Location = new System.Drawing.Point(63, 40);
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(100, 20);
+            this.txtBox1.TabIndex = 1;
+            // 
+            // lstBox1
+            // 
+            this.lstBox1.FormattingEnabled = true;
+            this.lstBox1.Location = new System.Drawing.Point(63, 95);
+            this.lstBox1.Name = "lstBox1";
+            this.lstBox1.Size = new System.Drawing.Size(120, 95);
+            this.lstBox1.TabIndex = 2;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(63, 196);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 3;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.lstBox1);
+            this.Controls.Add(this.txtBox1);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtBox1;
+        private System.Windows.Forms.ListBox lstBox1;
+        private System.Windows.Forms.Button btnRemove;
+    }
+}
+
---
+++ Me/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Me
+{
+    public partial class Form1 : Form
+    {
+        List<String> _items = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            lstBox1.DataSource = _items;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // The Add button was clicked.
+            _items.Add(txtBox1.Text);
+
+            // Change the DataSource.
+            lstBox1.DataSource = null;
+            lstBox1.DataSource = _items;
+          
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+
+            // The Remove button was clicked.
+            int selectedIndex = lstBox1.SelectedIndex;
+
+            try
+            {
+                // Remove the item in the List.
+                _items.RemoveAt(selectedIndex);
+            }
+            catch
+            {
+            }
+
+            lstBox1.DataSource = null;
+            lstBox1.DataSource = _items;
+        }
+    }
+}
